f5baafd78c5ef9fa0791162fb2089bd6ec980941
TriptiSharma047 <triptis1235@gmail.com>
2022-10-17 02:40:02 +0545

    modified setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,10 @@ setup(
    version='1.0',
    description='Test package for Python',
    license="MIT",
-   long_description=long_description,
    author='Tripti S.',
    author_email='triptis1235@gmail.com',
-   packages=['testpkg-taetris'],  #same as name
-   install_requires=[wheels], #external packages as dependencies
+   packages=[],  #same as name
+   install_requires=[], #external packages as dependencies
    scripts=[
             
            ]
