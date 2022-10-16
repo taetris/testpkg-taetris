212d583f92bc4022968bfc82842032f66d424ef4
TriptiSharma047 <triptis1235@gmail.com>
2022-10-16 23:05:01 +0545

    test1.py added to test dir

---
+++ tests/test1.py
@@ -0,0 +1,8 @@
+# Initial test file
+
+from sample1 import SampleClass as sample
+
+print(sample.lower("ALL CAPS"))
+print(sample.upper("all small"))
+print(sample.title("this IS a TRIAL."))
+print(sample.kebab("this is a trial"))
