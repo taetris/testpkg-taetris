818b6b84107193ba95e21478b432911175b4d489
taetris <triptis1235@gmail.com>
2022-10-30 21:40:55 +0545

    pytest marker implemented

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -2,20 +2,26 @@
 import pytest
 from src.sample1 import TextTransform as s
 
+@pytest.mark.parametrize("valInput, valOutput", [
+    ("a", "a"),
+    ("pYtesT fixture TESTING","pytest fixture testing"),
+    ("A: b c!", "a: b c!"),
+])
+
+def test_lower(valInput, valOutput): 
+    assert s.t_lower(valInput) == valOutput
+
 @pytest.fixture
-def valInput():
+def valInput2():
     return "pYtesT fixture TESTING"
 
-def test_lower(valInput): 
-    assert s.t_lower(valInput)== "pytest fixture testing"
+def test_upper(valInput2): 
+    assert s.t_upper(valInput2)=="PYTEST FIXTURE TESTING"
 
-def test_upper(valInput): 
-    assert s.t_upper(valInput)=="PYTEST FIXTURE TESTING"
+def test_title(valInput2): 
+    assert s.t_title(valInput2) == "Pytest Fixture Testing"
 
-def test_title(valInput): 
-    assert s.t_title(valInput) == "Pytest Fixture Testing"
-
-def test_kebab(valInput):
-    assert s.t_kebab(valInput) =="pytest-fixture-testing"
+def test_kebab(valInput2):
+    assert s.t_kebab(valInput2) =="pytest-fixture-testing"
 
 
