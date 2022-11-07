9dceae14a2b3ed4082e8bfe40751ffa52174f0a6
taetris <triptis1235@gmail.com>
2022-11-08 04:26:20 +0545

    image download added

--- src/news_scrape.py
+++ src/news_scrape.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-header = {"User Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36"}
 url = requests.get('https://www.sharesansar.com/newsdetail/investors-group-are-forewarning-that-the-brokers-license-suspension-would-result-in-them-protesting-on-the-streets-2022-11-07')
 html = BeautifulSoup(url.content, "html.parser")
 
