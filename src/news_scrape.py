98e483062436cfea5312223adaaab16bca1d1c73
taetris <triptis1235@gmail.com>
2022-11-08 04:23:24 +0545

    image downloader added

--- src/news_scrape.py
+++ src/news_scrape.py
@@ -4,17 +4,27 @@ from bs4 import BeautifulSoup
 header = {"User Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36"}
 url = requests.get('https://www.sharesansar.com/newsdetail/investors-group-are-forewarning-that-the-brokers-license-suspension-would-result-in-them-protesting-on-the-streets-2022-11-07')
 html = BeautifulSoup(url.content, "html.parser")
-         
+
+# TBD: extract only from the selected class    
 def content():
     if html.find('div', class_ = 'col-md-12'):
         paras = html.findAll('p')
         content = ""
         for para in paras:
-            content = content + para.get_text() + '\n\n'
+            content = content + para.get_text() 
         return content
 
-def image():
-    pass
+# TBD: find images only in the class
+def download_image():
+    imgs = html.find_all("img")[2]
+    imglink = imgs.attrs.get("src")
+    # print(imglink)
+
+    image = requests.get(imglink).content
+    f = r"files" + imglink [imglink.rfind("/"):]
+    
+    with open(f, "wb") as file:
+        file.write(image)
 
 def links():
     pass
@@ -23,6 +33,15 @@ def title():
     title = html.title.get_text()
     return title
 
-print(title(),"\n\n\n", content())
+def savefile():
+    toFile = ""
+    toFile = title() + '\n\n\n' + content()
 
+    f = open("files/sharesansar.txt", "w")
+    f.write(toFile)
+    f.close()
 
+    print("text copied to sharesansar.txt")
+
+# savefile()
+download_image()
