0075be890db6e9883fa5c49295b4b044cccdbbdd
taetris <triptis1235@gmail.com>
2022-11-09 04:13:09 +0545

    list made and dumped to python

--- src/laptop_scrape.py
+++ src/laptop_scrape.py
@@ -4,40 +4,39 @@ import json
 
 
 f = open("files/laptop_scrape.json", "w")
-laptop_info = {}
+
 url = requests.get('https://www.sastodeal.com/electronic/laptops.html')
 html = BeautifulSoup(url.content, "html.parser")
 
 mainpage = html.find(["ol"], class_ = "products list items product-items")
-
-
 laptops = mainpage.findAll(["div" ], class_ = 'product-item-info')
-f.write('[')
+
+laptop_list = []
+
 for laptop in laptops:
     laptop_name = laptop.find(["a"], class_ = "product-item-link").get_text()
     laptop_url = laptop.find(["a" ], class_ = 'product photo product-item-photo').attrs.get("href")
     price = laptop.find(['span'], class_ = 'price').get_text()
-    img_url = laptop.find(['img']).attrs.get("src") 
+    img_url_all = laptop.find(['img']).attrs.get("src") 
+
+    if "static.sastodeal.com" in img_url_all:
+        img_url = img_url_all
+
+    laptop_info = {'laptop_name': laptop_name,
+                    'laptop_url': laptop_url,
+                    'img_url': img_url,
+                    'price': price
+                    }
+
+    laptop_list.append(laptop_info)         
+
+    # print('laptop name:',laptop_name, '\n\n', 
+    # 'laptop_url: ',laptop_url, '\n\n', 'price: ', price, '\n\n')
+    # if "static.sastodeal.com" in img_url:
+    #     print('img_url: ', img_url, '\n\n')
 
 
-    laptop_info['laptop_name'] = laptop_name
-    laptop_info['laptop_url'] = laptop_url
-    laptop_info['price'] = price
-    
-    if "static.sastodeal.com" in img_url:
-        laptop_info['img_url'] = img_url
 
-
-    print('laptop name:',laptop_name, '\n\n', 
-    'laptop_url: ',laptop_url, '\n\n', 'price: ', price, '\n\n')
-    if "static.sastodeal.com" in img_url:
-        print('img_url: ', img_url, '\n\n')
-
-
-
-    json.dump(laptop_info, f)
-    f.write(',')
-    f.write('\n')
-f.write(']')
+json.dump(laptop_list, f, indent = 4)
 f.close()
 
