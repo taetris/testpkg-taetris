941d33dc5b621846e517167acd4741c74827a8e8
taetris <triptis1235@gmail.com>
2022-11-09 01:03:10 +0545

    data saved to json file

--- src/laptop_scrape.py
+++ src/laptop_scrape.py
@@ -3,7 +3,8 @@ import requests
 import json
 
 
-# f = open("files/laptop_scrape.json", "w")
+f = open("files/laptop_scrape.json", "a")
+laptop_info = {}
 url = requests.get('https://www.sastodeal.com/electronic/laptops.html')
 html = BeautifulSoup(url.content, "html.parser")
 
@@ -16,15 +17,25 @@ for laptop in laptops:
     laptop_name = laptop.find(["a"], class_ = "product-item-link").get_text()
     laptop_url = laptop.find(["a" ], class_ = 'product photo product-item-photo').attrs.get("href")
     price = laptop.find(['span'], class_ = 'price').get_text()
+    img_url = laptop.find(['img']).attrs.get("src") 
+
+
+    laptop_info['laptop_name'] = laptop_name
+    laptop_info['laptop_url'] = laptop_url
+    laptop_info['price'] = price
     
+    if "static.sastodeal.com" in img_url:
+        laptop_info['img_url'] = img_url
+
+
     print('laptop name:',laptop_name, '\n\n', 
     'laptop_url: ',laptop_url, '\n\n', 'price: ', price, '\n\n')
-    img_url = laptop.find(['img']).attrs.get("src")
     if "static.sastodeal.com" in img_url:
         print('img_url: ', img_url, '\n\n')
 
 
 
-# f.write(toFile)
-# f.close()
+    json.dump(laptop_info, f)
+    
+f.close()
 
