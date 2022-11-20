24121d13125d03f9b94bca9005d24f8b5cae10b2
taetris <triptis1235@gmail.com>
2022-11-20 22:50:42 +0545

    functions made and test written

---
+++ tests/test_sastodeal_scrape.py
@@ -0,0 +1,15 @@
+import pytest
+import src.sastodeal_scrape as s
+
+@pytest.fixture
+def input_file():
+    return "files/laptop_scrape.json"
+
+@pytest.mark.parametrize("first_laptop_name", [
+    ('\nDell G5 15 SE Gaming Laptop Ryzen 7 4800H / AMD RX 5600M 6144MB / 8GB RAM / 512GB SSD / 15.6" FHD 144Hz Display '),
+    ('\nDell G5-15 5511 | i7-11800H | 8GB | 512SSD | NVIDIA RTX 3050-4GB GDDR6 | FHD | Office 2019 ')
+    
+])
+
+def test_laptopScrape(input_file, first_laptop_name):
+    assert  s.laptopScrape(input_file)[0]["laptop_name"] == first_laptop_name
